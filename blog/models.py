71dad536ef366ac783084a583fba04c1528dfd68
narmuhamedov <kingradomir@gmail.com>
2026-07-24 18:50:28 +0600

    lesson2

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='blog/')
+    description = models.TextField(blank=True) # blank=True поле не обзятельное для заполнения
+    person_name = models.CharField(max_length=20, default='Иванов Иван')
+    url_blog = models.URLField(blank=True)
+    created_at = models.DateField(null=True, auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from . import models
+
+def blog_detail_view(request, id):
+    if request.method == 'GET':
+        blog_id = get_object_or_404(models.Blog, id=id)
+    return render(request, 'blog_detail.html', {'blog_id': blog_id})
+
+
+
+def blog_list_view(request):
+    if request.method == 'GET':
+        #query - запрос
+        blog_lst = models.Blog.objects.all().order_by('-id')
+    return render(request, 'blog_lst.html', {'blog_lst': blog_lst})
+
+
+
+
+
 
 def hello_world_view(request):
     if request.method == 'GET':
